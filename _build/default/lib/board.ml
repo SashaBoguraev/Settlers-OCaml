5baa7f42ad3b03d036c62980b47990fd853b31f5
Leo <lcd49@cornell.edu>
2023-10-22 22:11:51 -0400

    Updated build functions in player.ml
    - Leo Davies lcd49
    - changed build_road
    - changed build_settlement
    - changed build_city

--- _build/default/lib/board.ml
+++ _build/default/lib/board.ml
@@ -2,18 +2,12 @@ open Player
 
 type resource = Clay | Wood | Sheep | Ore | Wheat
 
-<<<<<<< HEAD
 type node = {border_one_resource: resource option; border_one_number: int option; border_two_resource: resource option; border_two_number: int option; border_three_resource: resource option; border_three_number: int option; is_settlement: bool; is_city: bool; player: Player.t option}
 type edge = {node1: node; node2: node; is_road: bool; player: Player.t option}
-=======
-type node = {border_one_resource: resource option; border_one_number: int option; border_two_resource: resource option; border_two_number: int option; border_three_resource: resource option; border_three_number: int option; is_settlement: bool; is_city: bool}
-type edge = {node1: node; node2: node; is_road: bool}
->>>>>>> main
 
 module type BoardType = sig
   type t
 
-<<<<<<< HEAD
   val initial_vertices: node option list list
   val initial_edges: edge list
 
@@ -23,17 +17,10 @@ module type BoardType = sig
 
   val build_settlment : t -> t
 
-=======
-  val initial: t
-
-  (* val build_road : t -> t
-  val build_settlment : t -> t
->>>>>>> main
   val build_city : t -> t *)
 end
 
 module SmallBoard: BoardType = struct
-<<<<<<< HEAD
   type t = node option list list * edge list
 
   module Play = Player
@@ -162,110 +149,6 @@ module SmallBoard: BoardType = struct
                         is_settlement = false; is_city = false; player = None}
 
   let initial_vertices = [
-=======
-  type t = (node option) list list
-
-  module Player_one = Player.empty
-  module Player_two = Player.empty
-  
-  let node_one: node = {border_one_resource = None; border_one_number = None;
-                        border_two_resource = None; border_two_number = None;
-                        border_three_resource = Some Sheep; border_three_number = Some 6;}
-  
-  let node_two: node = {border_one_resource = None; border_one_number = None;
-                        border_two_resource = None; border_two_number = None;
-                        border_three_resource = Some Wood; border_three_number = Some 4;}
-  
-  let node_three: node = {border_one_resource = None; border_one_number = None;
-                        border_two_resource = None; border_two_number = None;
-                        border_three_resource = Some Sheep; border_three_number = Some 6;}
-
-  let node_four: node = {border_one_resource = None; border_one_number = None;
-                        border_two_resource = Some Sheep; border_two_number = Some 6;
-                        border_three_resource = Some Wood; border_three_number = Some 4;}
-  
-  let node_five: node = {border_one_resource = None; border_one_number = None;
-                        border_two_resource = Some Wood; border_two_number = Some 4;
-                        border_three_resource = None; border_three_number = None;}
-  
-  let node_six: node = {border_one_resource = None; border_one_number = None;
-                        border_two_resource = Some Sheep; border_two_number = Some 6;
-                        border_three_resource = Some Clay; border_three_number = Some 3;}
-  
-  let node_seven: node = {border_one_resource = Some Sheep; border_one_number = Some 6;
-                        border_two_resource = Some Wood; border_two_number = Some 4;
-                        border_three_resource = None; border_three_number = None;}
-  
-  let node_eight: node = {border_one_resource = Some Wood; border_one_number = Some 4;
-                        border_two_resource = None; border_two_number = None;
-                        border_three_resource = Some Wood; border_three_number = Some 2;}
-  
-  let node_nine: node = {border_one_resource = None; border_one_number = None;
-                        border_two_resource = None; border_two_number = None;
-                        border_three_resource = Some Clay; border_three_number = Some 3;}
-
-  let node_ten: node = {border_one_resource = Some Sheep; border_one_number = Some 6;
-                        border_two_resource = Some Clay; border_two_number = Some 10;
-                        border_three_resource = None; border_three_number = None;}
-  
-  let node_eleven: node = {border_one_resource = Some Wood; border_one_number = Some 4;
-                        border_two_resource = None; border_two_number = None;
-                        border_three_resource = Some Wood; border_three_number = Some 2;}
-  
-  let node_twelve: node = {border_one_resource = None; border_one_number = None;
-                        border_two_resource = Some Wood; border_two_number = Some 2;
-                        border_three_resource = None; border_three_number = None;}
-
-  let node_thirteen: node = {border_one_resource = None; border_one_number = None;
-                        border_two_resource = Some Clay; border_two_number = Some 3;
-                        border_three_resource = None; border_three_number = None;}
-  
-  let node_fourteen: node = {border_one_resource = Some Clay; border_one_number = Some 3;
-                        border_two_resource = None; border_two_number = None;
-                        border_three_resource = Some Wheat; border_three_number = Some 5;}
-  
-  let node_fifteen: node = {border_one_resource = None; border_one_number = None;
-                        border_two_resource = Some Wood; border_two_number = Some 2;
-                        border_three_resource = Some Ore; border_three_number = Some 1;}
-
-  let node_sixteen: node = {border_one_resource = Some Wood; border_one_number = Some 2;
-                        border_two_resource = None; border_two_number = None;
-                        border_three_resource = None; border_three_number = None;}
-  
-  let node_seventeen: node = {border_one_resource = Some Clay; border_one_number = Some 3;
-                        border_two_resource = None; border_two_number = None;
-                        border_three_resource = Some Sheep; border_three_number = Some 6;}
-  
-  let node_eighteen: node = {border_one_resource = None; border_one_number = None;
-                        border_two_resource = Some Wheat; border_two_number = Some 5;
-                        border_three_resource = Some Ore; border_three_number = Some 1;}
-  
-  let node_nineteen: node = {border_one_resource = Some Wood; border_one_number = Some 2;
-                        border_two_resource = Some Ore; border_two_number = Some 1;
-                        border_three_resource = None; border_three_number = None;}
-  
-  let node_twenty: node = {border_one_resource = None; border_one_number = None;
-                        border_two_resource = Some Wheat; border_two_number = Some 5;
-                        border_three_resource = None; border_three_number = None;}
-  
-  let node_twentyone: node = {border_one_resource = Some Wheat; border_one_number = Some 5;
-                        border_two_resource = Some Ore; border_two_number = Some 1;
-                        border_three_resource = None; border_three_number = None;}
-
-  let node_twentytwo: node = {border_one_resource = Some Ore; border_one_number = Some 1;
-                        border_two_resource = None; border_two_number = None;
-                        border_three_resource = None; border_three_number = None;}
-  
-  let node_twentythree: node = {border_one_resource = Some Wheat; border_one_number = Some 5;
-                        border_two_resource = None; border_two_number = None;
-                        border_three_resource = None; border_three_number = None;}
-  
-  let node_twentyfour: node = {border_one_resource = Some Ore; border_one_number = Some 1;
-                        border_two_resource = None; border_two_number = None;
-                        border_three_resource = None; border_three_number = None;}
-
-  let initial = [
->>>>>>> main
     [None; Some node_three; Some node_four];
     [None; Some node_four; Some node_five];
     [None; Some node_one; Some node_six];
@@ -292,7 +175,6 @@ module SmallBoard: BoardType = struct
     [Some node_twentyone; Some node_twentytwo; None]
   ]
 
-<<<<<<< HEAD
   let initial_edges = [
     {node1 = node_one; node2 = node_three; is_road = false; player = None};
     {node1 = node_one; node2 = node_four; is_road = false; player = None};
@@ -327,6 +209,4 @@ module SmallBoard: BoardType = struct
 
   let board = (initial_vertices, initial_edges)
 
-=======
->>>>>>> main
 end
